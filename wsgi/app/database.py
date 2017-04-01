28c9dca47f5dd317065740361ba08fc203843eb4
Farid Guliyev <fg301@itl207.student.eecs.qmul.ac.uk>
2017-04-01 21:41:00 +0100

    Updates

--- wsgi/app/database.py
+++ wsgi/app/database.py
@@ -1,14 +1,35 @@
-from app import app
 from flask_pymongo import PyMongo
-import os
 from flask import jsonify
+from app import app
+import os
 
 app.config['MONGO_DBNAME'] = os.environ['OPENSHIFT_APP_NAME']
-app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL']+os.environ['OPENSHIFT_APP_NAME']
+app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL'] + os.environ['OPENSHIFT_APP_NAME']
+
 mongo = PyMongo(app)
 
-# some comment!
+
+@app.route('/database/methods', methods=['GET'])
+def get_db_methods_and_attributes():
+	return jsonify({'All methods and attributes on a flask mongodb object' : dir(mongo.db)})
+
+
+@app.route('/database/London/methods', methods=['GET'])
+def get_collection_methods_and_attributes():
+	return jsonify({'All methods and attributes on a flask mongodn collection object' : dir(mongo.db.London_Visitors)})
+
 
 @app.route('/database/collections', methods=['GET'])
-def get_all_databases():
+def get_all_collections():
 	return jsonify({'result' : mongo.db.collection_names()})
+
+@app.route('/database/London/sample', methods=['GET'])
+def get_sample_document():
+	collection = mongo.db.London_Visitors
+	document = collection.find_one()
+	output = []
+	output.append({attr:value for attr, value in document.iteritems() if attr!=u'_id'})
+	print(output)
+	print(dir(output))
+	return jsonify({'sample record' : output})
+
